9b04fc87d7d24252ed1abe5be1574a69ba88bf07
Prasong Aroonruviwat <psafx@hotmail.com>
2015-07-13 11:53:44 +0700

    fix spacing and bug for Thai language

--- js/languages/jquery.validationEngine-th.js
+++ js/languages/jquery.validationEngine-th.js
@@ -7,11 +7,11 @@
                 "required": { // Add your regex rules here, you can take telephone as an example
                     "regex": "none",
                     "alertText": "* กรุณากรอกข้อมูล",
-                    "alertTextCheckboxMultiple": "* กรุเลือกตัวเลือก",
+                    "alertTextCheckboxMultiple": "* กรุณาเลือกตัวเลือก",
                     "alertTextCheckboxe": "* กรุณาเลือก 1 ตัวเลือก",
                     "alertTextDateRange": "* กรุณาเลือกวันที่"
                 },
-                "requiredInFunction": { 
+                "requiredInFunction": {
                     "func": function(field, rules, i, options){
                         return (field.val() == "test") ? true : false;
                     },
@@ -37,7 +37,7 @@
                     "alertText": "* ข้อมูลมากสุด ",
                     "alertText2": " ตัวอักษร"
                 },
-		        "groupRequired": {
+                "groupRequired": {
                     "regex": "none",
                     "alertText": "* กรุณาเลือก 1 ตัวเลือกจากทั้งหมด",
                     "alertTextCheckboxMultiple": "* กรุณาเลือกตัวเลือก",
@@ -58,7 +58,7 @@
                 "future": {
                     "regex": "none",
                     "alertText": "* วันในอดีต "
-                },	
+                },
                 "maxCheckbox": {
                     "regex": "none",
                     "alertText": "* มากที่สุด ",
@@ -85,7 +85,7 @@
                 "email": {
                     // HTML5 compatible email regex ( http://www.whatwg.org/specs/web-apps/current-work/multipage/states-of-the-type-attribute.html#    e-mail-state-%28type=email%29 )
                     "regex": /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-                    "alertText": "* อีเมล์ไม่ถูกต้อง"
+                    "alertText": "* อีเมลไม่ถูกต้อง"
                 },
                 "fullname": {
                     "regex":/^([a-zA-Z]+[\'\,\.\-]?[a-zA-Z ]*)+[ ]([a-zA-Z]+[\'\,\.\-]?[a-zA-Z ]+)+$/,
@@ -104,22 +104,22 @@
                     "regex": /^[\-\+]?((([0-9]{1,3})([,][0-9]{3})*)|([0-9]+))?([\.]([0-9]+))?$/,
                     "alertText": "* ตัวเลขไม่ถูกต้อง"
                 },
-                "date": {                    
-                    //	Check if date is valid by leap year
-        			"func": function (field) {
-    					var pattern = new RegExp(/^(\d{4})[\/\-\.](0?[1-9]|1[012])[\/\-\.](0?[1-9]|[12][0-9]|3[01])$/);
-    					var match = pattern.exec(field.val());
-    					if (match == null)
-    					   return false;
-    	
-    					var year = match[1];
-    					var month = match[2]*1;
-    					var day = match[3]*1;					
-    					var date = new Date(year, month - 1, day); // because months starts from 0.
-    	
-    					return (date.getFullYear() == year && date.getMonth() == (month - 1) && date.getDate() == day);
-        			},                		
-			        "alertText": "* วันที่ไม่ถูกต้อง, ต้องเป็นรูปแบบ ปปปป-ดด-วว"
+                "date": {
+                    //    Check if date is valid by leap year
+                    "func": function (field) {
+                        var pattern = new RegExp(/^(\d{4})[\/\-\.](0?[1-9]|1[012])[\/\-\.](0?[1-9]|[12][0-9]|3[01])$/);
+                        var match = pattern.exec(field.val());
+                        if (match == null)
+                           return false;
+
+                        var year = match[1];
+                        var month = match[2]*1;
+                        var day = match[3]*1;
+                        var date = new Date(year, month - 1, day); // because months starts from 0.
+
+                        return (date.getFullYear() == year && date.getMonth() == (month - 1) && date.getDate() == day);
+                    },
+                    "alertText": "* วันที่ไม่ถูกต้อง, ต้องเป็นรูปแบบ ปปปป-ดด-วว"
                 },
                 "ipv4": {
                     "regex": /^((([01]?[0-9]{1,2})|(2[0-4][0-9])|(25[0-5]))[.]){3}(([0-1]?[0-9]{1,2})|(2[0-4][0-9])|(25[0-5]))$/,
@@ -129,6 +129,10 @@
                     "regex": /^(https?|ftp):\/\/(((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:)*@)?(((\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5]))|((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?)(:\d*)?)(\/((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)+(\/(([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)*)*)?)?(\?((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|[\uE000-\uF8FF]|\/|\?)*)?(\#((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|\/|\?)*)?$/i,
                     "alertText": "* ลิงค์ไม่ถูกต้อง"
                 },
+                "onlyNumber": {
+                    "regex": /^[0-9]+$/,
+                    "alertText": "* ตัวเลขเท่านั้น"
+                },
                 "onlyNumberSp": {
                     "regex": /^[0-9\ ]+$/,
                     "alertText": "* ตัวเลขเท่านั้น"
@@ -137,13 +141,13 @@
                     "regex": /^[a-zA-Z\ \']+$/,
                     "alertText": "* ตัวหนังสือเท่านั้น"
                 },
-				"onlyLetterAccentSp":{
+                "onlyLetterAccentSp":{
                     "regex": /^[a-z\u00C0-\u017F\ ]+$/i,
                     "alertText": "* ตัวหนังสือเท่านั้น"
                 },
                 "onlyLetterNumber": {
                     "regex": /^[0-9a-zA-Z]+$/,
-                    "alertText": "alertText": "* ไม่สามารถใช้อักษรพิเศษ"
+                    "alertText": "* ไม่สามารถใช้อักษรพิเศษ"
                 },
                 // --- CUSTOM RULES -- Those are specific to the demos, they can be removed or changed to your likings
                 "ajaxUserCall": {
@@ -153,7 +157,7 @@
                     "alertText": "* ข้อมูลถูกใช้ไปแล้ว",
                     "alertTextLoad": "* กำลังตรวจสอบข้อมูล"
                 },
-				"ajaxUserCallPhp": {
+                "ajaxUserCallPhp": {
                     "url": "phpajax/ajaxValidateFieldUser.php",
                     // you may want to pass extra data on the ajax call
                     "extraData": "name=eric",
@@ -172,35 +176,35 @@
                     // speaks by itself
                     "alertTextLoad": "* กำลังจรวจสอบข้อมูล"
                 },
-				 "ajaxNameCallPhp": {
-	                    // remote json service location
-	                    "url": "phpajax/ajaxValidateFieldName.php",
-	                    // error
-	                    "alertText": "* ชื่อนี้ถูกใช้ไปแล้ว",
+                 "ajaxNameCallPhp": {
+                        // remote json service location
+                        "url": "phpajax/ajaxValidateFieldName.php",
+                        // error
+                        "alertText": "* ชื่อนี้ถูกใช้ไปแล้ว",
                         // speaks by itself
                         "alertTextLoad": "* กำลังจรวจสอบข้อมูล"
-	            },
+                },
                 "validate2fields": {
                     "alertText": "* กรอกข้อมูล HELLO"
                 },
-	            //tls warning:homegrown not fielded 
+                //tls warning:homegrown not fielded
                 "dateFormat":{
                     "regex": /^\d{4}[\/\-](0?[1-9]|1[012])[\/\-](0?[1-9]|[12][0-9]|3[01])$|^(?:(?:(?:0?[13578]|1[02])(\/|-)31)|(?:(?:0?[1,3-9]|1[0-2])(\/|-)(?:29|30)))(\/|-)(?:[1-9]\d\d\d|\d[1-9]\d\d|\d\d[1-9]\d|\d\d\d[1-9])$|^(?:(?:0?[1-9]|1[0-2])(\/|-)(?:0?[1-9]|1\d|2[0-8]))(\/|-)(?:[1-9]\d\d\d|\d[1-9]\d\d|\d\d[1-9]\d|\d\d\d[1-9])$|^(0?2(\/|-)29)(\/|-)(?:(?:0[48]00|[13579][26]00|[2468][048]00)|(?:\d\d)?(?:0[48]|[2468][048]|[13579][26]))$/,
                     "alertText": "* วันที่ไม่ถูกต้อง"
                 },
-                //tls warning:homegrown not fielded 
-				"dateTimeFormat": {
-	                "regex": /^\d{4}[\/\-](0?[1-9]|1[012])[\/\-](0?[1-9]|[12][0-9]|3[01])\s+(1[012]|0?[1-9]){1}:(0?[1-5]|[0-6][0-9]){1}:(0?[0-6]|[0-6][0-9]){1}\s+(am|pm|AM|PM){1}$|^(?:(?:(?:0?[13578]|1[02])(\/|-)31)|(?:(?:0?[1,3-9]|1[0-2])(\/|-)(?:29|30)))(\/|-)(?:[1-9]\d\d\d|\d[1-9]\d\d|\d\d[1-9]\d|\d\d\d[1-9])$|^((1[012]|0?[1-9]){1}\/(0?[1-9]|[12][0-9]|3[01]){1}\/\d{2,4}\s+(1[012]|0?[1-9]){1}:(0?[1-5]|[0-6][0-9]){1}:(0?[0-6]|[0-6][0-9]){1}\s+(am|pm|AM|PM){1})$/,
+                //tls warning:homegrown not fielded
+                "dateTimeFormat": {
+                    "regex": /^\d{4}[\/\-](0?[1-9]|1[012])[\/\-](0?[1-9]|[12][0-9]|3[01])\s+(1[012]|0?[1-9]){1}:(0?[1-5]|[0-6][0-9]){1}:(0?[0-6]|[0-6][0-9]){1}\s+(am|pm|AM|PM){1}$|^(?:(?:(?:0?[13578]|1[02])(\/|-)31)|(?:(?:0?[1,3-9]|1[0-2])(\/|-)(?:29|30)))(\/|-)(?:[1-9]\d\d\d|\d[1-9]\d\d|\d\d[1-9]\d|\d\d\d[1-9])$|^((1[012]|0?[1-9]){1}\/(0?[1-9]|[12][0-9]|3[01]){1}\/\d{2,4}\s+(1[012]|0?[1-9]){1}:(0?[1-5]|[0-6][0-9]){1}:(0?[0-6]|[0-6][0-9]){1}\s+(am|pm|AM|PM){1})$/,
                     "alertText": "* รูปแบบวันที่และเวลาไม่ถูกต้อง",
                     "alertText2": "ต้องการรูปแบบตามนี้: ",
                     "alertText3": "ดด/วว/ปปปป hh:mm:ss AM|PM หรือ ",
                     "alertText4": "ปปปป-ดด-วว hh:mm:ss AM|PM"
-	            }
+                }
             };
-            
+
         }
     };
 
     $.validationEngineLanguage.newLang();
-    
+
 })(jQuery);
